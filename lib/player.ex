c51900c5e63ca6584071f55f981fd27a8c7d8361
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-10-05 11:41:00 -0300

    refactor: convert required keys to a module variable and order arguments alphabetically

--- lib/player.ex
+++ lib/player.ex
@@ -1,17 +1,16 @@
 defmodule ExMon.Player do
-  required_keys = [:life, :name, :move_rnd, :move_avg, :move_heal]
-  @enforce_keys required_keys
-  defstruct required_keys
-
   @max_life 100
+  @required_keys [:life, :move_avg, :move_heal, :move_rnd, :name]
+  @enforce_keys @required_keys
+  defstruct @required_keys
 
   def build(name, move_rnd, move_avg, move_heal) do
     %ExMon.Player{
       life: @max_life,
-      name: name,
       move_avg: move_avg,
       move_rnd: move_rnd,
-      move_heal: move_heal
+      move_heal: move_heal,
+      name: name
     }
   end
 end
