7af53f1613a5aed31d75e026c38ef00c710be384
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-10-05 11:36:51 -0300

    refactor: move life value to a module variable

--- lib/player.ex
+++ lib/player.ex
@@ -3,9 +3,11 @@ defmodule ExMon.Player do
   @enforce_keys required_keys
   defstruct required_keys
 
+  @max_life 100
+
   def build(name, move_rnd, move_avg, move_heal) do
     %ExMon.Player{
-      life: 100,
+      life: @max_life,
       name: name,
       move_avg: move_avg,
       move_rnd: move_rnd,
