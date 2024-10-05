32c448421a070baea41cd405e22486b15cf575ef
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-10-05 19:30:38 -0300

    feat: create initial version of make_move

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,7 @@
 defmodule ExMon do
+  alias ExMon.Game.Actions
   alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
 
   @computer_name "Robotinik"
 
@@ -11,5 +13,11 @@ defmodule ExMon do
     @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
+
+    Status.print_round_message()
+  end
+
+  def make_move(move) do
+    Actions.fetch_move(move)
   end
 end
